4a40fa80e72f500d1a96990d70abd17014c2088b
devToJu <msg.toju@outlook.de>
2023-03-28 16:55:16 +0200

    add API call to add a new Books

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import axios from "axios";
-import {Book} from "./model/Book";
+import {Book, NewBook} from "./models/Book";
+import AddBook from "./components/AddBook";
 
 function App() {
     const [books, setBooks] = useState<Book[]>([]);
 
-    useEffect(() => getBooks());
+    useEffect(() => getBooks(), []);
 
     const getBooks = () => {
         axios.get("api/books")
             .then(response => setBooks(response.data))
-            .catch();
+            .catch(reason => console.log(reason));
+    }
+
+    const addBook = (newBook: NewBook) => {
+        axios.post("api/books", newBook)
+            .then(response => setBooks([...books, response.data]))
+            .catch(reason => console.log(reason));
     }
 
     return (
         <div className="App">
             {
-                books.map(book => <div>{book.title}</div>)
+                books.map(book => <div key={book.isbn}>{book.title}</div>)
             }
+            <AddBook addNewBook={addBook} />
         </div>
     );
 }
---
+++ frontend/src/components/AddBook.tsx
@@ -0,0 +1,47 @@
+import React, {ChangeEvent, FormEvent, useState} from "react";
+import {NewBook} from "../models/Book";
+
+type Props = {
+    addNewBook: (book: NewBook) => void;
+}
+
+export default function AddBook(props: Props) {
+    const initBook: NewBook = {
+        title: "",
+        author: "",
+        cover: "HARDCOVER"
+    }
+
+    const [newBook, setNewBook] = useState<NewBook>(initBook);
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const attributeName = event.target.name;
+        const value = event.target.value;
+        const book = {...newBook, [attributeName]: value};
+        setNewBook(book);
+    }
+
+    const addBook = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        props.addNewBook(newBook);
+        setNewBook(initBook);
+    }
+
+    return(
+        <>
+            <form onSubmit={addBook}>
+                <input type="text"
+                       name="title"
+                       onChange={onChange}
+                       value={newBook.title}
+                />
+                <input type="text"
+                       name="author"
+                       onChange={onChange}
+                       value={newBook.author}
+                />
+                <button type="submit">Add Book</button>
+            </form>
+        </>
+    )
+}
