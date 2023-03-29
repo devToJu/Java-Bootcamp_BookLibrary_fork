1cff88b175f81360a14af9e627de773ef5fc2255
devToJu <msg.toju@outlook.de>
2023-03-29 14:15:13 +0200

    add api request to get book details

--- frontend/src/components/UpdateBook.tsx
+++ frontend/src/components/UpdateBook.tsx
@@ -1,9 +1,36 @@
 import {useParams} from "react-router-dom";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {Book} from "../models/Book";
 
 export default function UpdateBook() {
     const {isbn} = useParams()
+    const [book, setBook] = useState<Book>()
+    const [msg, setMsg] = useState("loading...")
+
+    useEffect(() => {
+        if(isbn) {
+           getBook()
+        }
+    }, [])
+
+    const getBook = () => {
+        axios.get('/api/books/' + isbn)
+            .then(response => setBook(response.data))
+            .catch(reason => {
+                console.error("Error", reason)
+                setMsg("Could not found Book")
+            })
+    }
 
     return (
-        <div>{isbn}</div>
+        book
+            ? <div>
+                <p>Titel: {book.title}</p>
+                <p>Autor: {book.author}</p>
+                <p>Cover: {book.cover}</p>
+                <p>ISBN: {book.isbn}</p>
+            </div>
+            : <p>{msg}</p>
     )
 }
